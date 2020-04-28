c81d0ebf86de8de590539767cdec5ce2a35dc206
Riku Komiya <824rikukomiya@gmail.com>
2020-04-28 13:40:00 +0900

    property-decorator導入

--- layouts/default.vue
+++ layouts/default.vue
@@ -64,30 +64,30 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class extends Vue {
+  clipped = false
+  drawer = false
+  fixed = false
+  items = [
+    {
+      icon: 'mdi-apps',
+      title: 'Welcome',
+      to: '/'
+    },
+    {
+      icon: 'mdi-chart-bubble',
+      title: 'Inspire',
+      to: '/inspire'
     }
-  }
+  ]
+
+  miniVariant = false
+  right = true
+  rightDrawer = false
+  title = 'Vuetify.js'
 }
 </script>
